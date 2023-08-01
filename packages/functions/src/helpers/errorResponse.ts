c2e7323b8dbbfa836954c9b5c0204835947ab9a5
Lukáš Niedoba <niedoba.lukas@gmail.com>
2023-08-01 07:29:42 +0200

    Return correct headers for error responses

--- packages/functions/src/helpers/errorResponse.ts
+++ packages/functions/src/helpers/errorResponse.ts
@@ -3,6 +3,7 @@ import NotFoundError from "../errors/notFoundError";
 export const createValidationErrorResponse = (issues: object[]) => {
     return {
         statusCode: 400,
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ errors: issues }),
     };
 };
@@ -10,6 +11,7 @@ export const createValidationErrorResponse = (issues: object[]) => {
 export const createNotFoundErrorResponse = (error: NotFoundError) => {
     return {
         statusCode: 404,
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ error: error.message }),
     };
 };
